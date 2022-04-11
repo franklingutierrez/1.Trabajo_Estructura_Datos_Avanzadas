4b351e93fec4d6c528ea61d7d914547438bfa8cd
guiratec <fgutierrezarizaca@gmail.com>
2022-04-11 18:23:08 -0500

    Agregar archivos de proyecto.

---
+++ 1.Trabajo_Estructura_Datos_Avanzadas/problema1.cpp
@@ -0,0 +1,16 @@
+//**************************//
+// Resolucion problema 9   //
+//*************************//
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+	cin.get();
+	return 0;
+}
