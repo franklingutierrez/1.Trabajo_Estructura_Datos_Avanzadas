7f8dd5aa9a30304772133bced0ae59db0270ab31
guiratec <fgutierrezarizaca@gmail.com>
2022-04-11 22:00:34 -0500

    Finalizado con Selector multiple

--- 1.Trabajo_Estructura_Datos_Avanzadas/problema1.cpp
+++ 1.Trabajo_Estructura_Datos_Avanzadas/problema1.cpp
@@ -7,19 +7,51 @@ using namespace std;
 
 int main()
 {
+	int i=0, b, m;
 
-	int i=0, j, result;
-	cout << "Los multiples de 2 son: " << endl;
+	cout << "Digite (2 o 5) para obtener sus multiplos y [3] salir: \n";
+	cout << "-> "; cin >> m;
+	switch (m)
+	{
+	case 2:cout << "El multiplo de 2 es:" << endl;
+		cout << "==================================="<<endl;
+		while (i <= 100)
+		{
+			i++;
+			if (i % 2 == 0)
+			{
+				cout << i << " ";
+			}
+		}
+		cout << "\n\n\n"; return main(); break;
+
+	case 5:cout << "El multiplo de 5 es:"<< endl;
+		cout << "===================================" << endl;
 	while (i <= 100)
 	{
 		i++;
-		if(i%2==0)
+		if (i % 5 == 0)
 		{
 			cout << i << " ";
 		}
-		
 	}
+		cout << "\n\n\n"; return main(); break;
+	case 3:cout << "Seguro que quiere salir....?  SI o NO\n";
+		cout << "\n 1 - SI";
+		cout << "\n 2 - NO \n\n";
+		cout << "-> "; cin >> b;
+		if (b==1)
+		{
+			return 0;
+		}
+		else
+		{
+			return main();
+		}break;
+	default: cout << "No escogiste ninguna de las 2 opciones sugeridas \n\n"; break;
 
+	}
+	
 
 	cin.get();
 	return 0;
