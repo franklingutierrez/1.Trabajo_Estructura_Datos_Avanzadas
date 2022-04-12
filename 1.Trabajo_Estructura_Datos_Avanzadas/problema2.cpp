7112e331d62eff15686c77525f25b9c89d4f7eb4
guiratec <fgutierrezarizaca@gmail.com>
2022-04-11 23:08:02 -0500

    Inicio problema2

---
+++ 1.Trabajo_Estructura_Datos_Avanzadas/problema2.cpp
@@ -0,0 +1,30 @@
+/**********************************************
+----------------(5).PROBLEMA 2-----------------
+
+Mostrar los n primeros términos de la sucesión:
+1,3,4,7,11.., Xn; n ,es ingresado por pantalla.
+-----------------------------------------------
+     FRANKLIN HIUISTONG GUTIERREZ ARIZACA
+**********************************************/
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+	int n, an;
+	n++;
+	while (i<sus)
+	{
+		
+	}
+
+
+
+
+
+
+	cin.get();
+	return 0;
+}
