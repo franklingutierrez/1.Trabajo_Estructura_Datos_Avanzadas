e0145485c09e34561021f4a557508129612ecfe9
guiratec <fgutierrezarizaca@gmail.com>
2022-04-11 19:16:09 -0500

    While  multipllo de 2

--- 1.Trabajo_Estructura_Datos_Avanzadas/problema1.cpp
+++ 1.Trabajo_Estructura_Datos_Avanzadas/problema1.cpp
@@ -1,14 +1,24 @@
-//**************************//
-// Resolucion problema 9   //
-//*************************//
-#include<iostream>
 
+//5. Mostrar los múltiplos de 2 y 5 entre 0 y 100
+
+#include<iostream>
+#include<conio.h>
 using namespace std;
 
 int main()
 {
 
-
+	int i=0, j, result;
+	cout << "Los multiples de 2 son: " << endl;
+	while (i <= 100)
+	{
+		i++;
+		if(i%2==0)
+		{
+			cout << i << " ";
+		}
+		
+	}
 
 
 	cin.get();
